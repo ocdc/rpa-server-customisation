52344aa408d51ccbdc0158ed4a35b7dadaab98c2
Owen <owencarruk@gmail.com>
2022-10-12 12:44:06 +0100

    Update rpa-server-customisation.ps1

--- rpa-server-customisation.ps1
+++ rpa-server-customisation.ps1
@@ -6,7 +6,7 @@ function Show-Menu {
     Write-Host "2: Create desktop shortcuts."
     Write-Host "3: Manual installers."
     Write-Host "4: Manual setup tasks."
-	Write-Host "5: Update manifests."
+    Write-Host "5: Update manifests."
     Write-Host "Q: Press 'Q' to quit."
 }
 
@@ -22,6 +22,7 @@ do
             &"$($PSScriptRoot)\scripts\copy-shortcuts.ps1"
         } '3' {
             Write-Host "Items below will need to be manually downloaded and installed." -BackgroundColor Blue
+	    Write-Host "Chrome url: https://www.google.com/intl/en/chrome/?standalone=1"
             Write-Host "Jump Desktop Connect url: https://jumpdesktop.com/connect/"
             Write-Host "UI.Vision modules url: https://ui.vision/rpa/x/download"
             Write-Host "UI.Vision Chrome extension url: https://chrome.google.com/webstore/detail/uivision-rpa/gcbalfbdmfieckjlnblleoemohcganoc"
@@ -29,8 +30,8 @@ do
             Write-Host "Default GIT path should be C:\Users\$($Env:USERPROFILE)\GitHub"
             Write-Host "Default UI.Vision path should be C:\Users\$($Env:USERPROFILE)\GitHub\uivision"
         } '5' {
-			&"$($PSScriptRoot)\scripts\scoop-update-manifests.ps1"
-		}
+            &"$($PSScriptRoot)\scripts\scoop-update-manifests.ps1"
+	}
     }
     pause
  }
