578986c00d987748456bf0d20db12898525d18b0
Owen <owencarruk@gmail.com>
2022-10-13 19:51:21 +0100

    Update rpa-server-customisation.ps1

--- rpa-server-customisation.ps1
+++ rpa-server-customisation.ps1
@@ -3,10 +3,11 @@ function Show-Menu {
     Write-Host "=========== System Customisation Options ==========="
     
     Write-Host "1: Install."
-    Write-Host "2: Create desktop shortcuts."
-    Write-Host "3: Manual installers."
-    Write-Host "4: Manual setup tasks."
-    Write-Host "5: Update manifests."
+    Write-Host "2: Post install tasks."
+    Write-Host "3: Create desktop shortcuts."
+    Write-Host "4: Manual installers."
+    Write-Host "5: Manual setup tasks."
+    Write-Host "6: Update manifests."
     Write-Host "Q: Press 'Q' to quit."
 }
 
@@ -19,17 +20,25 @@ do
         '1' {
             &"$($PSScriptRoot)\scripts\install.ps1"
         } '2' {
-            &"$($PSScriptRoot)\scripts\copy-shortcuts.ps1"
+            &"$($PSScriptRoot)\scripts\install-post-setup.ps1"
         } '3' {
+            &"$($PSScriptRoot)\scripts\copy-shortcuts.ps1"
+        } '4' {
             Write-Host "Items below will need to be manually downloaded and installed." -BackgroundColor Blue
-	    Write-Host "Chrome url: https://www.google.com/intl/en/chrome/?standalone=1"
+	      Write-Host "Chrome url: https://www.google.com/intl/en/chrome/?standalone=1"
             Write-Host "Jump Desktop Connect url: https://jumpdesktop.com/connect/"
             Write-Host "UI.Vision modules url: https://ui.vision/rpa/x/download"
             Write-Host "UI.Vision Chrome extension url: https://chrome.google.com/webstore/detail/uivision-rpa/gcbalfbdmfieckjlnblleoemohcganoc"
-        } '4' {
+            Write-Host "Open all URLs in default browser..." -BackgroundColor Red
+            Read-Host -Prompt "Press enter to continue or CTRL+C to quit"
+	      Start-Process "https://www.google.com/intl/en/chrome/?standalone=1"
+            Start-Process "https://jumpdesktop.com/connect/"
+            Start-Process "https://ui.vision/rpa/x/download"
+            Start-Process "https://chrome.google.com/webstore/detail/uivision-rpa/gcbalfbdmfieckjlnblleoemohcganoc"
+        } '5' {
             Write-Host "Default GIT path should be C:\Users\$($Env:USERPROFILE)\GitHub"
             Write-Host "Default UI.Vision path should be C:\Users\$($Env:USERPROFILE)\GitHub\uivision"
-        } '5' {
+        } '6' {
             &"$($PSScriptRoot)\scripts\scoop-update-manifests.ps1"
 	}
     }
